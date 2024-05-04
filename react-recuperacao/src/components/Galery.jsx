686a25650a39004f82e4c5098f7eecb458ce20a2
Ruan <130461902+ruantasca@users.noreply.github.com>
2024-05-04 16:00:53 -0300

    commit

--- react-recuperacao/src/components/Galery.jsx
+++ react-recuperacao/src/components/Galery.jsx
@@ -4,41 +4,46 @@ export default function Galery(){
     <section className="conteiner-galeria">
         <div className="b">
                 <h1 className="h1b">Livros Disponiveis</h1>
-            </div>
+        </div>
     <div className="galeria">
         
         <div className="imagem-conteiner">
             <img src="../public/livro1.webp" />
-            <p>1</p>
+            <p>Titulo</p>
+            <p>lançamento</p>
         </div>
         
         <div className="imagem-conteiner">
             <img src="../public/img2.jpg" />
-            <p>1</p>
+            <p>Titulo</p>
+            <p>lançamento</p>
         </div>
         
         <div className="imagem-conteiner">
             <img src="../public/img3.jpg" />
-            <p>1</p>
+            <p>Titulo</p>
+            <p>lançamento</p>
         </div>
-    </div>
-    <div className="galeria1">
+        
         <div className="imagem-conteiner">
             <img src="../public/img4.webp" />
-            <p>1</p>
+            <p>Titulo</p>
+            <p>lançamento</p>
         </div>
         
         <div className="imagem-conteiner">
             <img src="../public/img5.png" />
-            <p>1</p>
+            <p>Titulo</p>
+            <p>lançamento</p>
         </div>
         
         <div className="imagem-conteiner">
             <img src="../public/img6.jpg" />
-            <p>1</p>
+            <p>Titulo</p>
+            <p>lançamento</p>
         </div>
     
-    </div> 
+    </div>
     </section>
     )
 }
